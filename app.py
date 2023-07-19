7dde04787fe432eaca07c7d650905604e6d055f8
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-07-19 09:53:49 +0100

    writtten the cleanning function

--- app.py
+++ app.py
@@ -4,3 +4,18 @@ from nltk.corpus import stopwords
 import re 
 import nltk 
 from flask import Flask, rquest, render_template
+
+# cleaning the text data (removing all unnecessary characters)
+
+def clean_text(text):
+
+    # removing the "\" sign
+    text = re.sub("'\''","",text)
+
+    # remove special characters an leaving only text(both lower and upper)
+    text = re.sub("[^a-zA-Z]", " ", text)
+
+    # Conevrt text to lowercase
+    text = text.lower()
+
+    return text
